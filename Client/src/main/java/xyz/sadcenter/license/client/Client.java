b94b891c7acfa9d3dde0f6a8963599965877cf06
sadcenter <55924469+sadcenter@users.noreply.github.com>
2021-01-28 16:11:00 +0100

    Update Client.java

--- Client/src/main/java/xyz/sadcenter/license/client/Client.java
+++ Client/src/main/java/xyz/sadcenter/license/client/Client.java
@@ -13,23 +13,4 @@ public final class Client {
         new ClientServer().setup(host, port, token, licenseCallback);
     }
 
-    public static void main(String[] args) {
-        connect("127.0.0.1", 2138, "b449abcc-6172-11eb-ae93-0242ac130002", new LicenseCallback() {
-            @Override
-            public void correct(String token) {
-                System.out.println("correct " + token);
-            }
-
-            @Override
-            public void incorrect(String token) {
-                System.out.println("incorrect " + token);
-            }
-
-            @Override
-            public void disconnected(Throwable e) {
-                System.out.println("cant connect!");
-            }
-        });
-    }
-
 }
